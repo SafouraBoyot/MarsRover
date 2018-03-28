99862e04238827de588ab1efedf0aa99b1eacb26
ggsbv <ggsbv@protonmail.com>
2018-03-28 13:04:26 +0100

    GridShould.move_rover_based_on_multiple_commands(), make_rover_wrap_around_the_grid_on_the_y_axis(), make_rover_wrap_around_the_grid_on_the_x_axis() >> pass

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -5,24 +5,42 @@ public class Grid {
     public Grid(Dimensions dimensions) {
         this.dimensions = dimensions;
         grid = new int[dimensions.width()][dimensions.height()];
-
     }
 
-    public void move(Rover rover, String navigationInstruction) {
-        if (navigationInstruction.equals("R")) rover.rotateRight();
-        if (navigationInstruction.equals("L")) rover.rotateLeft();
-        if (navigationInstruction.equals("M")) {
-            Coordinate coordinate = rover.position().coordinate();
-            if (rover.position().direction().equals(Direction.NORTH))
-                rover.changeCoordinate(new Coordinate(coordinate.x(), coordinate.y() + 1));
-            if (rover.position().direction().equals(Direction.SOUTH))
-                rover.changeCoordinate(new Coordinate(coordinate.x(), coordinate.y() - 1));
-            if (rover.position().direction().equals(Direction.EAST))
-                rover.changeCoordinate(new Coordinate(coordinate.x()+1, coordinate.y() ));
-            if (rover.position().direction().equals(Direction.WEST))
-                rover.changeCoordinate(new Coordinate(coordinate.x()-1, coordinate.y()));
+    public void move(Rover rover, String navigationInstructions) {
+        for (Character navigationInstruction : navigationInstructions.toCharArray()) {
+            Position position = rover.position();
+
+            if (navigationInstruction.equals('R')) rover.rotateRight();
+            if (navigationInstruction.equals('L')) rover.rotateLeft();
+            if (navigationInstruction.equals('M')) {
+                rover.changeCoordinate(nextCoordinateFor(position));
+            }
+        }
+    }
+
+    private Coordinate nextCoordinateFor(Position position) {
+        int x = position.coordinate().x();
+        int y = position.coordinate().y();
+        int nextX = x;
+        int nextY = y;
+
+        if (position.direction().equals(Direction.NORTH)) {
+            nextY = y + 1 > dimensions.height() ? 1 : y + 1;
         }
 
+        if (position.direction().equals(Direction.SOUTH)) {
+            nextY = y - 1 < 1 ? dimensions.height() : y - 1;
+        }
 
+        if (position.direction().equals(Direction.EAST)) {
+            nextX = x + 1 > dimensions.width() ? 1 : x + 1;
+        }
+
+        if (position.direction().equals(Direction.WEST)) {
+            nextX = x - 1 < 1 ? dimensions.width() : x - 1;
+        }
+
+        return new Coordinate(nextX, nextY);
     }
 }
--- src/test/java/GridShould.java
+++ src/test/java/GridShould.java
@@ -4,7 +4,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -24,35 +23,67 @@ public class GridShould {
     @Test
     public void
     rotate_rover_right() {
-
-
         when(rover.position()).thenReturn(new Position(new Coordinate(1, 2), Direction.NORTH));
+
         grid.move(rover, "R");
 
         verify(rover).rotateRight();
-
     }
 
     @Test
     public void
     rotate_rover_left() {
-
         when(rover.position()).thenReturn(new Position(new Coordinate(1, 2), Direction.NORTH));
+
         grid.move(rover, "L");
 
         verify(rover).rotateLeft();
-
     }
 
     @Test public void
     move_rover(){
         when(rover.position()).thenReturn(new Position(new Coordinate(1, 2), Direction.NORTH));
+
         grid.move(rover, "M");
 
         verify(rover).changeCoordinate(new Coordinate(1,3));
-
-
     }
 
+    @Test
+    public void
+    move_rover_based_on_multiple_commands() {
+        Position positionBefore = new Position(new Coordinate(1, 2), Direction.NORTH);
+        Position positionAfter = new Position(new Coordinate(1, 2), Direction.EAST);
+        when(rover.position()).thenReturn(positionBefore)
+                .thenReturn(positionAfter);
+
+        grid.move(rover, "RM");
+
+        verify(rover).changeCoordinate(new Coordinate(2,2));
+    }
+
+    @Test
+    public void
+    make_rover_wrap_around_the_grid_on_the_y_axis() {
+        Position position = new Position(new Coordinate(5, 5), Direction.NORTH);
+
+        when(rover.position()).thenReturn(position);
+
+        grid.move(rover, "M");
+
+        verify(rover).changeCoordinate(new Coordinate(5,1));
+    }
+
+    @Test
+    public void
+    make_rover_wrap_around_the_grid_on_the_x_axis() {
+        Position position = new Position(new Coordinate(1, 3), Direction.WEST);
+
+        when(rover.position()).thenReturn(position);
+
+        grid.move(rover, "M");
+
+        verify(rover).changeCoordinate(new Coordinate(5,3));
+    }
 }
 
